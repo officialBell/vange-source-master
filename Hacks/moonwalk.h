fa7c9fd5e44f9656a9211e4e68f84d053081d343
20jjanssen <australiantcg@gmail.com>
2018-06-20 15:27:27 +1000

    Create moonwalk.h

---
+++ Hacks/moonwalk.h
@@ -0,0 +1,16 @@
+//
+//  moonwalk.hpp
+//  vHook
+//
+//  Created by Joshua Janssen on 19/06/2018.
+//  Copyright © 2018 ViKiNG. All rights reserved.
+//
+#include "main.h"
+
+#pragma once
+
+namespace MoonWalk
+{
+    //Hooks
+    void CreateMove(CUserCmd* cmd);
+}
